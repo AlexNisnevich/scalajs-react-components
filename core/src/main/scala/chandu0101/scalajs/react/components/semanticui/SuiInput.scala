70cd0a566fead3304aa2453ce9d217cbc937810f
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-10-27 23:03:16 -0700

    working on SuiInput

--- core/src/main/scala/chandu0101/scalajs/react/components/semanticui/SuiInput.scala
+++ core/src/main/scala/chandu0101/scalajs/react/components/semanticui/SuiInput.scala
@@ -2,20 +2,33 @@ package chandu0101.scalajs.react.components.semanticui
 
 import chandu0101.macros.tojs.JSMacro
 import chandu0101.macros.tojs.JSMacro._
+import chandu0101.scalajs.react.components.sui._
 import japgolly.scalajs.react.{React, ReactComponentU_, ReactElement, ReactNode}
 
 import scala.scalajs.js
+import scala.scalajs.js.|
 
 case class SuiInput(
-  key:      js.UndefOr[String]        = js.undefined,
-  ref:      js.UndefOr[String]        = js.undefined,
-  as:       js.UndefOr[String]        = js.undefined,
-  defaultValue: js.UndefOr[String]    = js.undefined,
-  placeholder:  js.UndefOr[String]    = js.undefined,
-  `type`:   js.UndefOr[String]        = js.undefined,
-  value:    js.UndefOr[String]        = js.undefined,
-  label:    js.UndefOr[ReactElement]  = js.undefined,
-  labelPosition: js.UndefOr[String]   = js.undefined
+  key:            js.UndefOr[String]                                        = js.undefined,
+  ref:            js.UndefOr[SuiInput => Unit]                              = js.undefined,
+  as:             js.UndefOr[String | (Seq[ReactNode] => ReactComponentU_)] = js.undefined,
+  action:         js.UndefOr[Boolean | (ReactNode | js.Object)]             = js.undefined,
+  actionPosition: js.UndefOr[LeftRight]                                     = js.undefined,
+  className:      js.UndefOr[String]                                        = js.undefined,
+  disabled:       js.UndefOr[Boolean]                                       = js.undefined,
+  error:          js.UndefOr[Boolean]                                       = js.undefined,
+  focus:          js.UndefOr[Boolean]                                       = js.undefined,
+  fluid:          js.UndefOr[Boolean]                                       = js.undefined,
+  icon:           js.UndefOr[Boolean | (ReactNode | js.Object)]             = js.undefined,
+  iconPosition:   js.UndefOr[LeftRight]                                     = js.undefined,
+  inverted:       js.UndefOr[Boolean]                                       = js.undefined,
+  input:          js.UndefOr[ReactNode | js.Object]                         = js.undefined,
+  label:          js.UndefOr[ReactNode | js.Object]                         = js.undefined,
+  labelPosition:  js.UndefOr[LeftRight]                                     = js.undefined,
+  loading:        js.UndefOr[Boolean]                                       = js.undefined,
+  size:           js.UndefOr[Size]                                          = js.undefined,
+  transparent:    js.UndefOr[Boolean]                                       = js.undefined,
+  `type`:         js.UndefOr[String]                                        = js.undefined
 ) {
   def apply(children: ReactNode*) = {
     val props = JSMacro[SuiInput](this)
